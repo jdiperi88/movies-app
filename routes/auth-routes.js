8ec2ac88ff391435af9473cc4a68c716be77a2c6
BiCo26 <bianca.cortes26@gmail.com>
2017-08-20 23:52:23 -0400

    add routes/auth-routes.js

---
+++ routes/auth-routes.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const authRouter = express.Router();
+const passport = require('../services/auth/local');
+const authHelpers = require('../services/auth/auth-helpers');
+const usersController = require('../controllers/users-controller');
+
+authRouter.get('/login', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/login', {
+    currentPage: 'login', // this is specific to the movies app bc of the CSS setup
+  });
+});
+
+authRouter.get('/register', authHelpers.loginRedirect, (req, res) => {
+  res.render('auth/register', {
+    currentPage: 'register', // see above
+  });
+});
